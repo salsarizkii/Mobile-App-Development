8aaef9d8eaf913b2a0d4597e6c53441b124be09b
salsarizki <salsarizkisaputrii@gmail.com>
2024-03-21 14:35:47 +0700

    PAB WEEK 3

---
+++ Week 3/PPAB-03_L0122147_SalsaRizkiSaputri/SourceCode/ExplisitIntentSendData/app/src/main/java/com/l0122147/salsarizki/explisitintentsenddata/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.l0122147.salsarizki.explisitintentsenddata
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button // Add this import statement
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnProfile: Button = findViewById(R.id.btn_profile)
+        btnProfile.setOnClickListener(this) // jika error, alt+enter dan pilih yang "Let .."
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_profile -> {
+
+            }
+        }
+    }
+}
