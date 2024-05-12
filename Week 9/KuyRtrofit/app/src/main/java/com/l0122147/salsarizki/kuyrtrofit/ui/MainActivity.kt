325d3d1894c7dd264102c0ea4ca1967777204268
salsarizki <salsarizkisaputrii@gmail.com>
2024-05-12 15:35:18 +0700

    commit

--- Week 9/KuyRtrofit/app/src/main/java/com/l0122147/salsarizki/kuyrtrofit/ui/MainActivity.kt
+++ Week 9/KuyRtrofit/app/src/main/java/com/l0122147/salsarizki/kuyrtrofit/ui/MainActivity.kt
@@ -1,21 +1,59 @@
 package com.l0122147.salsarizki.kuyrtrofit.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.l0122147.salsarizki.kuyrtrofit.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.l0122147.salsarizki.kuyrtrofit.data.response.UserResponse
+import com.l0122147.salsarizki.kuyrtrofit.data.retrofit.ApiConfig
+import com.l0122147.salsarizki.kuyrtrofit.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // Declare binding as lateinit var
+
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater) // Initialize binding correctly
+
+        setContentView(binding.root)
+
+        adapter = UserAdapter(this@MainActivity, arrayListOf())
+
+        binding.rvUser.adapter = adapter
+        binding.rvUser.setHasFixedSize(true)
+        binding.rvUser.layoutManager = LinearLayoutManager(this)
+
+        getUsers()
+    }
+
+    private fun getUsers() {
+        ApiConfig.apiService().getUsers().enqueue(object : Callback<ArrayList<UserResponse>> {
+            override fun onResponse(
+                call: Call<ArrayList<UserResponse>>,
+                response: Response<ArrayList<UserResponse>>
+            ) {
+                if (response.isSuccessful) {
+                    val data = response.body()
+                    data?.let {
+                        setData(it)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<UserResponse>>, t: Throwable) {
+                Log.d("Error", "" + t.stackTraceToString())
+            }
+        })
+    }
+
+    private fun setData(data: ArrayList<UserResponse>) {
+        adapter.setData(data)
     }
 }
---
+++ Week 9/KuyRtrofit/app/src/main/java/com/l0122147/salsarizki/kuyrtrofit/ui/UserAdapter.kt
@@ -0,0 +1,54 @@
+package com.l0122147.salsarizki.kuyrtrofit.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.l0122147.salsarizki.kuyrtrofit.R
+import com.l0122147.salsarizki.kuyrtrofit.data.response.UserResponse
+
+class UserAdapter (
+    private val context: Context,
+    private val data: ArrayList<UserResponse>
+): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+    class UserViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvId = view.findViewById<TextView>(R.id.tvId)
+        val tvName = view.findViewById<TextView>(R.id.tvName)
+        val tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+        val tvPhone = view.findViewById<TextView>(R.id.tvPhone)
+        val tvEmail = view.findViewById<TextView>(R.id.tvEmail)
+        val tvWeb = view.findViewById<TextView>(R.id.tvWeb)
+        val cvUser = view.findViewById<CardView>(R.id.cvUser)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemView = layoutInflater.inflate(R.layout.item_user, parent, false)
+
+        return UserViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.tvId.text = data.get(position).id.toString()
+        holder.tvName.text = data.get(position).name
+        holder.tvUsername.text = data.get(position).username
+        holder.tvPhone.text = data.get(position).phone
+        holder.tvEmail.text = data.get(position).email
+        holder.tvWeb.text = data.get(position).website
+        holder.cvUser.setOnClickListener {
+            Toast.makeText(context, "" + data.get(position).username, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    fun setData(newData: ArrayList<UserResponse>) {
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
+    }
+}
