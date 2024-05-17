1581008ca7fd1c6cd6693363ea2c6d82e0ebac8b
salsarizki <salsarizkisaputrii@gmail.com>
2024-05-17 14:09:28 +0700

    commit

---
+++ responsi1/app/src/main/java/com/l0122147/salsarizki/responsi1/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.l0122147.salsarizki.responsi1.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
