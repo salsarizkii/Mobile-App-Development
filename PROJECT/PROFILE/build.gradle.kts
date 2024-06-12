65fd2941b73da8053057f33f9dae6026cc38c5b3
Salsa Rizki <salsarizkisaputri@student.uns.ac.id>
2024-06-12 13:59:59 +0700

    commit

--- PROJECT/PROFILE/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
-}
